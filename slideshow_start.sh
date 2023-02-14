6c4fa59ee35d7a20c3759a190af297bc71761f55
Науменко Тарас <taras.naumenko@gazprombank.ru>
2023-02-14 21:57:12 +0500

    fix globbing

--- slideshow_start.sh
+++ slideshow_start.sh
@@ -23,7 +23,7 @@ countString=$(wc -l $FILE)
 countArr=(${countString// / })
 COUNT=${countArr[0]}
 
-if (($COUNTER >= $COUNT)); then
+if ((COUNTER >= COUNT)); then
   echo "reset counter"
   COUNTER=0
 fi
@@ -32,16 +32,16 @@ LAST_LINE=$(expr $COUNT - $COUNTER)
 
 while read -r line; do
   COUNTER=$(expr $COUNTER + 1)
-  echo $COUNTER >${basePath}/${prefix}_counter.lst
-  echo $line >${basePath}/${prefix}_current.lst
+  echo "$COUNTER" >"${basePath}/${prefix}_counter.lst"
+  echo "$line" >"${basePath}/${prefix}_current.lst"
 
   rm -rdf "${TMPDIR}${line}"
   mkdir -p "${TMPDIR}${line}"
   while read -r file; do
-    fileArr=(${file//// })
-    convert "$file" -size 1300x100! -background none caption:"${fileArr[3]} ${fileArr[4]}" -geometry +10+10 -composite "$TMPDIR$file"
+    IFS='/' read -r -a fileArr <<<"$file"
+    convert "$file" -size 1300x150! -background none caption:"${fileArr[3]} ${fileArr[4]}" -geometry +10+10 -composite "$TMPDIR$file"
   done < <(find "${line}" -type f -name "*.jpg" -o -iname "*.JPG")
   $(DISPLAY=:1 imv-x11 -r -f -t$timeShow -x -s full "${TMPDIR}${line}")
   #$(imv-x11 -r -f -t$timeShow -x -s full "${TMPDIR}${line}")
   rm -rdf "${TMPDIR}${line}"
-done < <(tail -n $LAST_LINE $FILE)
+done < <(tail -n "${LAST_LINE}" "${FILE}")
